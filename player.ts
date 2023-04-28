a941391936a49185ae7cb155c840a84715cc514a
sam-morgan <andersonhenderson45@gmail.com>
2023-04-28 10:50:29 -0400

    Cleaned up code and revised comments to be more concise

--- player.ts
+++ player.ts
@@ -19,39 +19,39 @@ export class PromptPlayer implements Player {
 }
 
 export class BotPlayer implements Player {
-    // These numbers are -/+ 1 than the actual min and max values so that the
-    // actual end points are accessible. If this small leeway wasn't given,
-    // then the bot would never guess 1 or 1024, but would get extremely close
-    // every time. Theoretically these could be set to any arbitrary number
-    // close to the end points, but I chose to make them as close as possible.
+    // The minimum and maximum values the random number can be. This sets the
+    // initial bounds for the first guess.
     private minThreshold: number = 1;
     private maxThreshold: number = 1024;
 
-    // This is the initial guess (Result == undefined).
-    // Since there is nothing to base this guess off of, the best option is
-    // to simply pick the value in the middle. Since the randomizer can
-    // choose from between 1 and 1025 (not including 1025), the middle
-    // would be 1024 / 2 = 512.
-    private guess: number = Math.floor((this.maxThreshold + this.minThreshold) / 2);
+    // The actual number to be returned by next()
+    private guess: number;
 
     next(prevResult: Result): number {
-        // Previous guess failed, so try to determine the next guess base on
+        // If the previous guess failed, try to determine the next guess base on
         // whether it was too low or too high.
         if (prevResult instanceof TryAgain)
         {
             switch(prevResult.hint)
             {
+                // If the previously guessed number was too high...
                 case 'high':
+                    // ...now you know that the bounds is less than that guess.
                     this.maxThreshold = this.guess - 1;
                     break;
+                // Similarly, if the previously guessed number was too low...
                 case 'low':
+                    // ...now you know that the bounds greater than that guess.
                     this.minThreshold = this.guess + 1;
                     break;
             }
         }
 
+        // This gets the average of the two bounds for the statistically most
+        // probable answer and then floors it.
         this.guess = Math.floor((this.maxThreshold + this.minThreshold) / 2);
 
+        // Finally return the guess.
         return this.guess;
     }
 }
